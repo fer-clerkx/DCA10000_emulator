649fbc33e8550e9d8544aa8c1caa9ec8462839fe
Fer Clerkx <f.clerkx@student.fontys.nl>
2024-02-22 13:11:43 +0100

    Implemented response functionality, use different socket for sending response.

--- DCA1000Emulator.py
+++ DCA1000Emulator.py
@@ -5,6 +5,7 @@ class DCA1000Emulator:
 
     CONFIG_FILE = "ConfigFile.json"
     COMMAND_REQ_MAX_SIZE = 512
+    CLI_IP_ADDR = "169.254.235.8"
 
     # Packet format constants
     PACKET_HEADER = 0xA55A
@@ -22,41 +23,50 @@ class DCA1000Emulator:
         print(f"Parsing {self.CONFIG_FILE}.")
         with open(self.CONFIG_FILE) as config_file:
             json_config = json.load(config_file)
-        self.ip_addr = json_config["DCA1000Config"]\
+        ip_addr = json_config["DCA1000Config"]\
             ["ethernetConfig"]["DCA1000IPAddress"]
-        self.config_port = json_config["DCA1000Config"]\
+        config_port = json_config["DCA1000Config"]\
             ["ethernetConfig"]["DCA1000ConfigPort"]
 
+        self.dca_address = (ip_addr, config_port)
+        self.cli_address = (self.CLI_IP_ADDR, config_port)
+
         # Create UDP sockets
-        self.config_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.config_socket.setblocking(False)
+        self.config_rx_socket = socket.socket(socket.AF_INET,
+                                              socket.SOCK_DGRAM)
+        self.config_rx_socket.setblocking(False)
+        self.config_tx_socket = socket.socket(socket.AF_INET,
+                                                socket.SOCK_DGRAM)
 
     def run(self):
         try:
             # Bind sockets
-            self.config_socket.bind((self.ip_addr, self.config_port))
-            print(f"UDP socket bound to {self.ip_addr}:{self.config_port}.")
+            self.config_rx_socket.bind(self.dca_address)
+            print(f"UDP socket bound to {self.dca_address[0]}:"
+                                      f"{self.dca_address[1]}.")
 
             while True:
                 self.receive_packet()
                 self.check_header()
                 self.process()
                 self.check_footer()
+                self.send_response()
         except KeyboardInterrupt:
             print("Program interrupted by user.")
-            self.config_socket.close()
-            print(f"Closed UDP socket.")
+            self.config_rx_socket.close()
+            self.config_tx_socket.close()
+            print(f"Closed UDP sockets.")
 
     def receive_packet(self):
         while True:
             try:
                 (self.buffer, client_address) = \
-                    self.config_socket.recvfrom(self.COMMAND_REQ_MAX_SIZE)
+                    self.config_rx_socket.recvfrom(self.COMMAND_REQ_MAX_SIZE)
             except BlockingIOError:
                 pass
             else:
                 print(f"Received packet from {client_address}.")
-                if client_address == (self.ip_addr, self.config_port):
+                if client_address == self.dca_address:
                     break
                 else:
                     print("Incorrect client address, packet dropped.")
@@ -88,6 +98,14 @@ class DCA1000Emulator:
         if self.read_bytes(2) != self.PACKET_FOOTER:
             print("Incorrect packet footer, packet dropped.")
             # TODO: throw exception
+
+    def send_response(self):
+        packet = (self.PACKET_HEADER.to_bytes(2, 'little')
+                  + self.command_code.to_bytes(2, 'little')
+                  + self.status.to_bytes(2, 'little')
+                  + self.PACKET_FOOTER.to_bytes(2, 'little'))
+        print("Sending response packet.")
+        self.config_tx_socket.sendto(packet, self.cli_address)
 
     def read_bytes(self, num_bytes):
         value = int.from_bytes(self.buffer[:num_bytes], 'little')
